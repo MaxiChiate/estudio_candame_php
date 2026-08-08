10ad57765e011cb79a29390cfe79c5b77c25da38
Máximo Chiatellino <mchiatellino@itba.edu.ar>
2026-08-08 20:54:39 -0300

    Error view

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpException;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
@@ -41,6 +43,45 @@ $errorMiddleware = $app->addErrorMiddleware(
     (bool) ($_ENV['APP_DEBUG'] ?? false),
     true,
     true,
+);
+
+// Reemplaza la pagina de error HTML por defecto de Slim (blanca, sin estilos del
+// sitio) por error.html.twig, para que un 404 o un 500 se vean como el resto del
+// sitio en vez de una pantalla generica de framework.
+$errorMiddleware->setDefaultErrorHandler(
+    function (Request $request, Throwable $exception, bool $displayErrorDetails) use ($app, $twig, $basePath) {
+        $statusCode = $exception instanceof HttpException ? $exception->getCode() : 500;
+        if ($statusCode < 400 || $statusCode > 599) {
+            $statusCode = 500;
+        }
+
+        if ($statusCode === 404) {
+            $errorTitle = 'Pagina no encontrada';
+            $errorMessage = 'La pagina que busca no existe o fue movida.';
+        } else {
+            $errorTitle = 'Ocurrio un error';
+            $errorMessage = 'Ocurrio un error inesperado. Por favor intente nuevamente en unos minutos.';
+        }
+
+        if ($displayErrorDetails) {
+            $errorMessage .= ' (' . $exception->getMessage() . ')';
+        }
+
+        if ($statusCode >= 500) {
+            error_log('Unhandled exception: ' . $exception->getMessage());
+        }
+
+        $response = $app->getResponseFactory()->createResponse($statusCode);
+
+        return $twig->render($response, 'error.html.twig', [
+            'pageTitle' => $errorTitle . ' - Estudio Candame',
+            'metaDescription' => $errorMessage,
+            'metaKeywords' => '',
+            'statusCode' => $statusCode,
+            'errorTitle' => $errorTitle,
+            'errorMessage' => $errorMessage,
+        ]);
+    }
 );
 
 (require APP_PATH . '/config/routes.php')($app, $twig);
